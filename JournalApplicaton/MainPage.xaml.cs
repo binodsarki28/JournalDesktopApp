7048f0582fd95068e440bf198b9fe13ae8ad654c
binodsarki28 <binodsarki69@gmail.com>
2025-12-21 17:10:30 +0545

    added

---
+++ JournalApplicaton/MainPage.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace JournalApplicaton
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
