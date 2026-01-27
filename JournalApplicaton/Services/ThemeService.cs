99d1bd02fe0fb6fc08457ea9bcf6c9580b900069
binodsarki28 <binodsarki69@gmail.com>
2026-01-27 21:14:49 +0545

    refine ui of calendar and journal and implement the search filter

---
+++ JournalApplicaton/Services/ThemeService.cs
@@ -0,0 +1,18 @@
+﻿public class ThemeService
+{
+    public bool IsDark { get; private set; }
+
+    public event Action? OnThemeChanged;
+
+    public void SetTheme(bool isDark)
+    {
+        IsDark = isDark;
+        OnThemeChanged?.Invoke();
+    }
+
+    public void ToggleTheme()
+    {
+        IsDark = !IsDark;
+        OnThemeChanged?.Invoke();
+    }
+}
