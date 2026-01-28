3eed47683fd2ad26947bc9257b2f7e4c63427826
binodsarki28 <binodsarki69@gmail.com>
2026-01-28 16:18:22 +0545

    refine code

--- JournalApplicaton/Services/IUserService.cs
+++ JournalApplicaton/Services/IUserService.cs
@@ -7,7 +7,4 @@ public interface IUserService
 {
     Task<ServiceResult<UserDisplayModel>> RegisterUserAsync(UserViewModel viewModel);
     Task<ServiceResult<UserDisplayModel>> LoginUserAsync(string username, string password);
-    Task<ServiceResult<UserDisplayModel>> GetUserByIdAsync(int id);
-    Task<ServiceResult<UserDisplayModel>> UpdateUserAsync(int id, UserViewModel viewModel);
-    Task<ServiceResult<bool>> DeleteUserAsync(int id);
 }
